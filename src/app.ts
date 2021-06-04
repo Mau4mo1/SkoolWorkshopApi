b9b66caebc6ea3a22746c9cefcee3e2ca7ca7dfb
Dani <Daniheyl@live.nl>
2021-06-04 10:37:32 +0200

    Auto stash before merge of "feature/SK-121-Models" and "origin/feature/SK-121-Models"

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,34 @@
+// import express from 'express';
+// import path from 'path';
+// import cookieParser from 'cookie-parser';
+// import logger from 'morgan';
+// import router from './routes/indexRoutes';
+
+// const app = express();
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/api', router);
+
+// export default app;
+
+
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import router from './routes/indexRoutes';
+import { createConnection } from 'typeorm';
+import WorkshopCategory from './models/WorkshopCategory';
+import Registration from './models/Registration';
+import Round from './models/Round';
+import WorkshopPicture from './models/WorkshopPicture';
+import Workshop from './models/Workshop';
+import Participant from './models/Participant';
 
 // Test
 import WorkshopRepo from './data/WorkshopRepo';
