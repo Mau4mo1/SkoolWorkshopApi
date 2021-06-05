c47e12b84dfcbdb94c1425a36704bd9fe6845fc3
Dani <Daniheyl@live.nl>
2021-06-05 18:32:12 +0200

    Update SK-103 pictureRoutes

--- src/routes/pictureRoutes.ts
+++ src/routes/pictureRoutes.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import PictureController from '../controllers/PictureController';
+
+const pictureController = new PictureController();
 
 const router = express.Router();
 // get one picture
 router.get('/:id');
 // get all pictures
-router.get('/');
+router.get('/',pictureController.getAll);
 
 export default router;
