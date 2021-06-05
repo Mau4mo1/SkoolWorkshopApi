34eb72fb358ac6320e6ebe6cac3ca89b2613c875
Dani <Daniheyl@live.nl>
2021-06-05 18:32:35 +0200

    ADD SK-21 Picture Model

---
+++ src/models/Picture.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export default class Picture {
+
+	@PrimaryGeneratedColumn()
+    Id: number;
+
+    @Column()
+    Rubric: string;
+
+    @Column()
+    FileName: string;
+
+    @Column()
+    ContentType: string;
+    
+    @Column()
+    Blob: string;
+
+    @Column()
+    ThumbNail: string;
+}
