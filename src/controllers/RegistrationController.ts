5dac3376f4530a4a82f970347e8a10e88b7af660
Maurice <74411014+Mauricederidder@users.noreply.github.com>
2021-06-06 14:42:29 +0200

    Update SK-122 RegistrationController response refactoring

--- src/controllers/RegistrationController.ts
+++ src/controllers/RegistrationController.ts
@@ -3,40 +3,23 @@ import CrudController from './CrudController';
 import { Connection, createConnection } from 'typeorm';
 import Registration from '../models/Registration';
 import RegistrationRepo from '../data/RegistrationRepo';
+import Controller from './Controller';
 
 var logger = require('tracer').console();
 // implement the repo from the data
 const registrationRepo = new RegistrationRepo();
 
-export default class RegistrationController implements CrudController<Registration> {
+export default class RegistrationController extends Controller implements CrudController<Registration> {
     create(req: Request, res: Response, next: any): void {
         throw new Error('Method not implemented.');
     }
     async getById(req: any, res: any, next: any): Promise<void> {
-        const result = await registrationRepo.getById(req.params.registrationId);
-
-		if (result != undefined) {
-			res.status(200).json({
-				result: result,
-			});
-		} else {
-			res.status(500).json({
-				result: 'Nothing found',
-			});
-		}
+		console.log(this);
+		super.response(res,await registrationRepo.getById(req.params.registrationId));
     }
     async getAll(req: any, res: any, next: any): Promise<void> {
-        const result = await registrationRepo.getAll();
-
-		if (result != undefined) {
-			res.status(200).json({
-				result: result,
-			});
-		} else {
-			res.status(500).json({
-				result: 'Nothing found',
-			});
-		}
+		console.log(this);
+		super.response(res,await registrationRepo.getAll());
     }
     update(req: Request, res: Response, next: any): void {
         throw new Error('Method not implemented.');
