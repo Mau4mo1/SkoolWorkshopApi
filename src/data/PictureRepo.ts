7184b470418f35fec60e9e70b35552a11e46fac9
Rico Schouten <r.schouten1@student.avans.nl>
2021-06-06 15:44:13 +0200

    Fix SK-148 PictureRepo

--- src/data/PictureRepo.ts
+++ src/data/PictureRepo.ts
@@ -4,20 +4,23 @@ import CrudRepo from './CrudRepo';
 import Repo from './Repo';
 
 export default class PictureRepo extends Repo implements CrudRepo<Picture> {
-    getAll(): Promise<Picture[]> {
+    async getAll(): Promise<Picture[]> {
         return this.execute((connection: Connection) => connection.getRepository(Picture).find());
     }
-    getById(id: number): Promise<Picture> {
-        throw new Error('Method not implemented.');
-    }
-    create(object: Picture): void {
-        throw new Error('Method not implemented.');
-    }
-    update(id: number, object: Picture): void {
-        throw new Error('Method not implemented.');
-    }
-    delete(id: number): void {
+
+    async getById(id: number): Promise<Picture> {
         throw new Error('Method not implemented.');
     }
 
+    async create(object: Picture): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+
+    async update(id: number, object: Picture): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+
+    async delete(id: number): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
 }
