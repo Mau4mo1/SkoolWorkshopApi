628a1053692d8b436393b12e3f30b9742a26e818
Maurice <74411014+Mauricederidder@users.noreply.github.com>
2021-06-08 19:02:40 +0200

    Add SK-103 WorkshopRoute getranslations route

--- src/routes/workshopRoutes.ts
+++ src/routes/workshopRoutes.ts
@@ -9,5 +9,6 @@ router.get('/', workshopController.getAll);
 router.get('/:workshopId', workshopController.validation, workshopController.getById);
 router.get('/:workshopId/:workshopRoundId');
 router.get('/:workshopId/pictures', workshopController.getPictures);
+router.get('/:workshopId/translations', workshopController.getTranslations);
 
 export default router;
