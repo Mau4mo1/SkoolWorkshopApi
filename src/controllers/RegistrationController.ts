9e358c29098af7eb759d27e5bbe46b6dc94ec160
Rico Schouten <r.schouten1@student.avans.nl>
2021-06-08 13:32:01 +0200

    Update SK-148 Controller abstractions with typing

--- src/controllers/RegistrationController.ts
+++ src/controllers/RegistrationController.ts
@@ -1,3 +1,4 @@
+import { Request, Response, NextFunction } from 'express';
 import CrudController from './CrudController';
 import RegistrationRepo from '../data/RegistrationRepo';
 import Controller from './Controller';
@@ -5,25 +6,23 @@ import Controller from './Controller';
 const registrationRepo = new RegistrationRepo();
 
 export default class RegistrationController extends Controller implements CrudController {
-    async getById(req: any, res: any, next: any): Promise<void> {
-		console.log(this);
-		super.response(res,await registrationRepo.getById(req.params.registrationId));
-    }
-    async getAll(req: any, res: any, next: any): Promise<void> {
-		console.log(this);
-		super.response(res,await registrationRepo.getAll());
-    }
+  async getAll(request: Request, response: Response, next: NextFunction): Promise<void> {
+    super.respond(response, await registrationRepo.getAll());
+  }
 
-	async create(req: Request, res: Response, next: any): Promise<void> {
-        throw new Error('Method not implemented.');
-    }
+  async getById(request: Request, response: Response, next: NextFunction): Promise<void> {
+    super.respond(response, await registrationRepo.getById(parseInt(request.params.registrationId)));
+  }
 
-    async update(req: Request, res: Response, next: any): Promise<void> {
-        throw new Error('Method not implemented.');
-    }
+	async create(request: Request, response: Response, next: NextFunction): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
 
-    async delete(req: Request, res: Response, next: any): Promise<void> {
-        throw new Error('Method not implemented.');
-    }
-    
+  async update(request: Request, response: Response, next: NextFunction): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  async delete(request: Request, response: Response, next: NextFunction): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
 }
--- src/controllers/WorkshopController.ts
+++ src/controllers/WorkshopController.ts
@@ -1,3 +1,4 @@
+import { Request, Response, NextFunction } from 'express';
 import CrudController from './CrudController';
 import WorkshopRepo from '../data/WorkshopRepo';
 import Controller from './Controller';
@@ -5,23 +6,23 @@ import Controller from './Controller';
 const workshopRepo = new WorkshopRepo();
 
 export default class WorkshopController extends Controller implements CrudController {
-	async getById(req: any, res: any, next: any): Promise<void> {
-		super.response(res,await workshopRepo.getById(req.params.work));
+	async getAll(request: Request, response: Response, next: NextFunction): Promise<void> {
+		super.respond(response, await workshopRepo.getAll());
+	}
+	
+	async getById(request: Request, response: Response, next: NextFunction): Promise<void> {
+		super.respond(response, await workshopRepo.getById(parseInt(request.params.workshopId)));
 	}
 
-	async getAll(req: any, res: any, next: any): Promise<void> {
-		super.response(res,await workshopRepo.getAll());
-	}
-
-	async create(req: Request, res: Response, next: any): Promise<void> {
+	async create(request: Request, response: Response, next: NextFunction): Promise<void> {
 		throw new Error('Method not implemented.');
 	}
 
-	async update(req: Request, res: Response, next: any): Promise<void> {
+	async update(request: Request, response: Response, next: NextFunction): Promise<void> {
 		throw new Error('Method not implemented.');
 	}
 
-	async delete(req: Request, res: Response, next: any): Promise<void> {
+	async delete(request: Request, response: Response, next: NextFunction): Promise<void> {
 		throw new Error('Method not implemented.');
 	}
 }
