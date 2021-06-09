bcf405eac6b5b6b7cebc6b3a07b9a69e94ed3a83
Mau4mo1 <mauricederidder@outlook.com>
2021-06-09 16:02:46 +0200

    Add SK-103 category to indexRoutes

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -4,10 +4,12 @@ import organisationRouter from './organisationRoutes';
 import userRouter from './userRoutes';
 import registrationRouter from './registrationRoutes';
 import pictureRouter from './pictureRoutes';
+import categoryRouter from './categoryRoutes';
 
 const router = express.Router();
 
 router.use('/workshop', workshopRouter);
+router.use('/category', categoryRouter);
 router.use('/organisation', organisationRouter);
 router.use('/user', userRouter);
 router.use('/registrations', registrationRouter);
