925acb3121b3199dde2a7967d015139bce256458
Maurice <74411014+Mauricederidder@users.noreply.github.com>
2021-06-10 13:49:02 +0200

    Update SK-216 controller result to be json object

--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -7,7 +7,7 @@ import { isNumber } from 'util';
 export default class Controller {
     async respond(response: Response, result: any): Promise<void> {
         if (result != undefined) {
-			response.status(200).json(result);
+			response.status(200).json({result});
 		} else {
 			response.status(500).json({
 				error: 'Iets in de server heeft zijn taak niet juist volbracht, ik ga geen namen noemen'
