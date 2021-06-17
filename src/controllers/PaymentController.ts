f1389c4949affeab6dd98529225164258c45217e
TimdeLaater <tdelaater@gmail.com>
2021-06-17 14:26:57 +0200

    Update SK-175 working payment

--- src/controllers/PaymentController.ts
+++ src/controllers/PaymentController.ts
@@ -3,42 +3,47 @@ import CrudController from './CrudController';
 import { createMollieClient, SequenceType, PaymentMethod } from '@mollie/api-client';
 import Controller from './Controller';
 
-const mollieClient = createMollieClient({ apiKey: 'test_pQRK7dmHB3DJN2c3eQFFRN4fDA84DH' });
+const mollieClient = createMollieClient({ apiKey: 'test_W3pnKvRm2cGtzQUdzEhnCVWWEMG7p5' });
+
 
 
 export default class PaymentController extends Controller {
 
-    async validation(request: Request, response: Response, next: NextFunction) : Promise<void>{
 
-    }
-	async getAll(request: Request, response: Response, next: NextFunction): Promise<void>{
-
-    }
 	async getById(request: Request, response: Response, next: NextFunction): Promise<void>{
-        // mollieClient.payments.get(payment.id)
-        // .then(payment => {
-        //   // E.g. check if the payment.isPaid()
-        // })
-        // .catch(error => {
-        //   // Handle the error
-        // });
+        mollieClient.payments.get(request.body.id)
+        .then(payment => {
+            if(payment.isPaid){
+                response.status(200).json("Da hedde goed gedaan kut");
+            }
+          // E.g. check if the payment.isPaid()
+
+        })
+        .catch(error => {
+            response.status(500).json("foutje kut")
+          // Handle the error
+        });
     }
 	async create(request: Request, response: Response, next: NextFunction): Promise<void>{
         console.log(request.body.money);
-        
         mollieClient.payments.create({
             amount: {
-              value:    "10.99",
+              value:    request.body.money,
               currency: "EUR"
             },
+            sequenceType : SequenceType.oneoff,
             method : PaymentMethod.ideal,
-            description: "Hoe kut is dat",
-            redirectUrl: "https://google.com"
+            
+            description: request.body.description,
+            redirectUrl: "https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data",
+            webhookUrl: "http://127.0.0.1:3000/api/v1/payment/"
           })
             .then(payment => {
-                if(payment.isPaid()){
-                    response.status(200).json();
-                } else{
+                if(payment.isOpen()){
+                    response.status(200).json(payment._links.checkout);
+                    console.log(payment._links.checkout, payment.id);
+                } 
+                    else{
                     response.status(400).json({error: "Betalen kut"});
                 }
                 
--- src/routes/paymentRoutes.ts
+++ src/routes/paymentRoutes.ts
@@ -4,6 +4,7 @@ import PaymentController from '../controllers/PaymentController';
 const router = express.Router();
 const paymentController = new PaymentController();
 router.get('/');
+router.post('/', paymentController.getById);
 router.post('/create', paymentController.create);
 
 export default router;
